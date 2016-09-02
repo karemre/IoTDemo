6f63549d36429a6dc61043ab2ecfd046b12279c4
Mohamed Mohamed <mmohamed@us.ibm.com>
2016-09-02 13:23:42 -0700

    updated server ip
    Signed-off-by: Emre Yigitoglu <yigitogl@us.ibm.com>

--- HeatControl.java
+++ HeatControl.java
@@ -7,21 +7,18 @@ import java.util.concurrent.TimeUnit;
 public class HeatControl {
 
             static String hostIp;
-            
+
             public void connect(String source) throws Exception {
 
-                
             }
 
             public static class MyThread implements Runnable {
-                
-                   String source; 
+                   String source;
                    public MyThread(String s) {
                        source = s;
                    }
 
                    public void run() {
-                       
                     // Make connection and initialize streams
                        // Socket socket = new Socket(source, 32769);
                        // if(socket.isConnected())
@@ -31,28 +28,24 @@ public class HeatControl {
 
                        // Consume the initial welcoming messages from the server
 
-                       
                        while(true){
                            int temp = 60 + (int) (Math.random() * 30);
-                           
                            String actuate = "";
                            if(temp < 75)
                                actuate = "heater_on";
                            else if (temp > 80)
                                actuate = "cooler_on";
-                           else 
+                           else
                                actuate = "normal";
-                           
-                  
-                           String date = (new SimpleDateFormat("HH:mm:ss")).format((Calendar.getInstance()).getTime());     
-    
+
+                           String date = (new SimpleDateFormat("HH:mm:ss")).format((Calendar.getInstance()).getTime());
                            String result = date + "-" + source + "-" + temp + "-" + hostIp + "-" + actuate ;
-                               
+
+
                            try {
-                               
-                               String command = "curl -i --data " + result + " -X POST http://9.1.196.15:8000/results";
-                               Process p = Runtime.getRuntime().exec(command);             
-                               p.waitFor(); 
+                               String command = "curl -i --data " + result + " -X POST http://9.1.75.126:8000/results";
+                               Process p = Runtime.getRuntime().exec(command);
+                               p.waitFor();
                                BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
                                String line = "";
                                while ((line = reader.readLine())!= null){
@@ -61,7 +54,6 @@ public class HeatControl {
                            } catch (Exception ex){
                                ex.printStackTrace();
                            }
-			
 			   try {
 				TimeUnit.SECONDS.sleep(1);
 			   } catch (InterruptedException e) {
@@ -71,13 +63,13 @@ public class HeatControl {
                        }
                    }
                 }
-            
+
             public static void main(String[] args) throws Exception {
                 //HeatControl client = new HeatControl();
                 hostIp = args[0];
                 for(int i = 1; i < args.length ; i++){
                     //client.connect(args[0]);
-                    
+
                     Runnable r = new MyThread(args[i]);
                     new Thread(r).start();
                 }
